64f4eaaebd5dbea4bd9cda15cc9945a750fe8dcc
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-07-13 23:23:07 +0530

    created express server

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,15 @@
-const express=require('express');
+const express = require("express");
 
-
-const app=express();
+const app = express();
+app.use((req, res) => {
+  res.send("hello from the server!");
+});
+app.use("/hello",(req, res) => {
+  res.send("hello from the server!");
+});
+app.use("/test",(req, res) => {
+  res.send("hello from the server!");
+});
+app.listen(3000, () => {
+  console.log("Server is successfully listening on port 3000");
+});
