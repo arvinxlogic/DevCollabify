83293f804f0b925dcbd58287d9522478120bb783
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-07-14 19:14:39 +0530

    playing with routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,45 @@
 const express = require("express");
 
 const app = express();
-app.use((req, res) => {
-  res.send("hello from the server!");
-});
-app.use("/hello",(req, res) => {
-  res.send("hello from the server!");
-});
-app.use("/test",(req, res) => {
-  res.send("hello from the server!");
-});
+
+// app.use("/user",(req,res)=>{
+//   res.send("hahaha");
+// })
+
+// app.get("/ab+c",(req,res)=>{
+//   res.send({firstName:"Arvind", lastnName:"Singh"})
+// })
+// a?bc
+// a
+//*fly$
+// app.get("/user",(req,res)=>{
+//   res.send({firstName:"Arvind", lastnName:"Singh"})
+// })
+
+app.get("/user/:userId/:name/:password",(req,res)=>{
+  console.log(req.params);
+  res.send({
+    firstName:"Arv",lastnName:"singh"
+  })
+})
+// app.post("/user", (req,res)=>{
+//   console.log("Save data to the database");
+//   res.send("Data successfully saved to the database")
+// })
+// app.use("/hello", (req, res) => {
+//   res.send("hello hello hello server!");
+// });
+// app.delete("/user",(req,res)=>{
+//   res.send("Deleted successfully")
+// })
+// app.use("/test", (req, res) => {
+//   res.send("hello from the server!");
+// });
+
+// app.use("/",(req, res) => {
+//   res.send("hello from the server!");
+// });
+
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
 });
