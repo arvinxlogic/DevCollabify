d1a3106514ebe903069f124032dde095994d4c1c
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-07-15 18:45:24 +0530

    error handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 
 const app = express();
-const {adminAuth}=require("./middlewares/auth");
+const { adminAuth } = require("./middlewares/auth");
 
+app.get("/getUserData", (req, res) => {
+  try{
 
-app.use("/admin",adminAuth);
-app.get("/user", (req,res)=>{
-  res.send("user data sent");
-  
-})
-
+    throw new Error("dvaldsf");
+    res.send("User Data sent");
+  } catch(err){
+    res.status(500).send("some eror come");
+  }
+});
+app.use("/", (err, req, res, next) => {
+  if (err) res.status(500).send("something went wrong");
+});
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
 });
